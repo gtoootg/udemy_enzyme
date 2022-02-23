a1ea865678882521f76e78b82ccc2412333ff952
gtoootg0131 <gotosan0131@yahoo.co.jp>
2022-02-23 20:55:53 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function App() {
+  const [count, setCount] = useState(0)
   return (
     <div className="App" data-test="component-app">
-      
+      <h1 id={count} data-test="counter-display">
+        counter is currently&nbsp;
+        <span data-test="count">{count}</span>
+      </h1>
+      <button data-test="increment-button" onClick={()=>setCount(count+1)}>Increment counter</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable testing-library/await-async-query */
 import Enzyme, {shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react'
@@ -5,19 +6,42 @@ import App from './App'
 
 Enzyme.configure({ adapter: new Adapter() });
 
+const setup = ()=>shallow(<App/>)
+
+const findByTestAttr = (wrapper, val)=> wrapper.find(`[data-test='${val}']`)
+ 
 test('renders learn react link', () => {
-  const wrapper = shallow(<App/>)
-  expect(wrapper.find("[data-test='component-app']")).toHaveLength(1)
+  const wrapper = setup()
+  // eslint-disable-next-line testing-library/await-async-query
+  const appComponent = findByTestAttr(wrapper,"component-app")
+  expect(appComponent).toHaveLength(1)
 });
 
-test('renders increment button', () => {
-  
-});
 
 test('renders counter display', () => {
   
 });
 
 test('counter display starts at 0', () => {
-  
+  const wrapper = setup()
+  const count = findByTestAttr(wrapper, "count").text()
+  expect(count).toBe("0")
+});
+
+test('click button increments counter display', () => {
+  const wrapper = setup()
+  const button = findByTestAttr(wrapper, "increment-button")
+  button.simulate('click')
+
+  const count = findByTestAttr(wrapper, "count").text()
+  expect(count).toBe("1")
+});
+
+test('id of h1 tag changes after increment', () => {
+  const wrapper = setup()
+  const button = findByTestAttr(wrapper, "increment-button")
+  button.simulate('click')
+
+  const id = findByTestAttr(wrapper, "counter-display")
+  expect(id.get(0).props.id).toBe(1)
 });
