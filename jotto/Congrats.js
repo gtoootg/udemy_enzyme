595fc6d839409c176b61da30b08b75cfa02cdabe
gtoootg0131 <gotosan0131@yahoo.co.jp>
2022-03-02 21:48:17 +0100

    added Congrats.js

---
+++ jotto/Congrats.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default function Congrats(props){
+    if(props.success){
+        return(
+        <div data-test ="component-congrats">
+            <span data-test="congrats-message">
+                congratulations!
+            </span>
+        </div>
+        )
+    } else{
+        return(
+            <div data-test ="component-congrats">
+                <span data-test="congrats-message">
+                    
+                </span>
+            </div>
+        )
+    }
+   
+}
---
+++ jotto/Congrats.test.js
@@ -0,0 +1,34 @@
+/* eslint-disable testing-library/await-async-query */
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react'
+import Congrats from './Congrats'
+
+import {findByTestAttr} from '../test/testUtils'
+import { text } from 'express';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const setup = (props={}) => {
+    return shallow(<Congrats {...props}/>)
+}
+
+test('no error', ()=>{
+    const wrapper = setup()
+    const component = findByTestAttr(wrapper,'component-congrats')
+    expect(component.length).toBe(1)
+})
+
+test('renders no text when success prop is false', ()=>{
+    const wrapper = setup({success : false})
+    const component = findByTestAttr(wrapper,'component-congrats')
+    expect(component.text()).toBe('')
+})
+
+test('renders text when success prop is true', ()=>{
+    const wrapper = setup({success : true})
+    const component = findByTestAttr(wrapper,'component-congrats')
+    expect(component.text()).toBe('congratulations!')
+})
+
+test('renders non-empty congrats message when success prop is true', ()=>{})
