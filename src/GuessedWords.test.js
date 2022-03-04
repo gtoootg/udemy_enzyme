b1c4137cd41f4ea7fcaa4235200102f5621bd466
gtoootg0131 <gotosan0131@yahoo.co.jp>
2022-03-04 14:23:07 +0100

    update guessedword

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -1,10 +1,14 @@
+/* eslint-disable testing-library/await-async-query */
 import Enzyme, {shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+
 import React from 'react'
 import GuessedWords from './GuessedWords'
 
 import {findByTestAttr, checkProps} from '../test/testUtils'
 import { text } from 'express';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 const defaultProps = {
     guessedWords: [{guessedWord: 'train', letterMatchCount: 3}],
@@ -21,3 +25,18 @@ test('does not throw warnings with expected props',()=>{
 
 })
 
+describe('if there are no words guessed', ()=>{
+    let wrapper
+    beforeEach(()=>{
+        wrapper = setup({guessedWords: []})
+    })
+    test('render without error', ()=>{
+        // eslint-disable-next-line testing-library/await-async-query
+        const component = findByTestAttr(wrapper, 'component-guessed-words')
+        expect(component.length).toBe(1)
+    })
+    test('render instructions to guess a word', ()=>{
+        const instructions = findByTestAttr(wrapper, 'guess-instructions')
+        expect(instructions.text().length).not.toBe(0)
+    })
+})
